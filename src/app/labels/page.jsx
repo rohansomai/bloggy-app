a0a69e28277f53a138333e02325c51f2767efc52
Rohan Somai <rohansomai6@gmail.com>
2023-07-09 02:17:30 +0530

    Added Sidebar component

---
+++ src/app/labels/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Labels = () => {
+  return <div> Labels Component works </div>;
+};
+
+export default Labels;
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Posts = () => {
+  return <div> Posts Component works </div>;
+};
+
+export default Posts;
